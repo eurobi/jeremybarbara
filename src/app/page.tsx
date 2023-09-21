be066f72abcd90cc1dbfe49faa7c0d06d06ba671
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2023-09-21 17:53:48 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,115 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    <main>
+      <div className="main-container-div">
+        <div className="header-div">
+          <h1 className="header-name" id='header-first-name'>J E R E M Y</h1>
+          <h1 className="header-name" id='header-last-name'>B A R B A R A</h1>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="ticker-wrapper">
+        <div className="logo-ticker-div">
+          <img className="ticker-img" src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Business_Insider_Logo.svg/2560px-Business_Insider_Logo.svg.png'></img>
+          <img className="ticker-img" src='https://adworld.online/static/media/logo-dark.cce61d4f.svg'></img>
+          <img className="ticker-img" src='https://streamys.org/wp-content/uploads/2012/08/Streamy-Awards_Linear.png'></img>
+          <img className="ticker-img" id='ticker-img-shorty' src='https://d3f8w85pjd4o8c.cloudfront.net/static/images/assets/logos/Shorty21_Logo_v2_Horiztonal_Awards_Shorty_Black.png'></img>
+          <img className="ticker-img" id='ticker-img-flatiron' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/FS_wiki.png/640px-FS_wiki.png'></img>
+          <img className="ticker-img" src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Business_Insider_Logo.svg/2560px-Business_Insider_Logo.svg.png'></img>
+          <img className="ticker-img" src='https://adworld.online/static/media/logo-dark.cce61d4f.svg'></img>
+          <img className="ticker-img" src='https://streamys.org/wp-content/uploads/2012/08/Streamy-Awards_Linear.png'></img>
+          <img className="ticker-img" id='ticker-img-shorty' src='https://d3f8w85pjd4o8c.cloudfront.net/static/images/assets/logos/Shorty21_Logo_v2_Horiztonal_Awards_Shorty_Black.png'></img>
+          <img className="ticker-img" id='ticker-img-flatiron' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/FS_wiki.png/640px-FS_wiki.png'></img>
+        </div>
+        </div>
+        <div className="title-div">
+          <div className="title-selection-div" id='selected-title'>
+            <div className="title-selection-text-div">
+              <h1>INFLUENCER</h1>
+              <h1>MARKETER</h1>
+            </div>
+          </div>
+          <div className="title-selection-plus">
+              <h1>+</h1>
+            </div>
+          <div className="title-selection-div">
+            <div className="title-selection-text-div">
+              <h1>SOFTWARE</h1>
+              <h1>ENGINEER</h1>
+            </div>
+          </div>
+        </div>
+        <div className="arrow-container-div">
+          <div className="title-arrow"></div>
+        </div>
+        <div className="bio-div">
+          <p className="bio-text"><i>
+          An experienced marketer who has built award winning Influencer Marketing teams and strategies at multiple early stage companies; working with thousands of creators on high impact direct response and branded campaigns, garnering billions of views accross channels.
+          </i></p>
+        </div>
+        <div className="company-div">
+          <div className="section-header-div">
+            <h1 className="section-header-text">B R A N D S</h1>
+          </div>
+          <div className="company-item-div" id='company-item-seatgeek'>
+            <div id='seatgeek-logo-div'>
+              <img id='seatgeek-logo' src='/Secondary.png'></img>
+            </div>
+            <div className="company-desc-div">
+              <div className="company-desc-item">
+                <h1>Ambasadors</h1>
+                <h1>3,000+</h1>
+              </div>
+              <div className="company-desc-item">
+                <h1>Ticket Sales</h1>
+                <h1>$120M</h1>
+              </div>
+              <div className="company-desc-item">
+                <h1>ROAS</h1>
+                <h1>125%</h1>
+              </div>
+              <div className="company-desc-item">
+                <h1>Followers</h1>
+                <h1>1M+</h1>
+              </div>
+            </div>
+          </div>
+          <div className="company-item-div" id='company-item-current'>
+            <div id='current-logo-div'>
+              <img id='current-logo' src='/Current_wordmark_white.png'></img>
+            </div>
+            <div className="company-desc-div">
+            <div className="company-desc-item">
+            <h1>Views</h1>
+                <h1>1B+</h1>
+              </div>
+              <div className="company-desc-item">
+                <h1>Signups</h1>
+                <h1>300,000+</h1>
+              </div>
+              <div className="company-desc-item">
+                <h1>Videos</h1>
+                <h1>1,200+</h1>
+              </div>
+              <div className="company-desc-item">
+                <h1>Yearly Spend</h1>
+                <h1>$15M+</h1>
+              </div>
+            </div>
+          </div>
+          <div className="company-item-div" id='company-item-sundays'>
+            <div id='sundays-logo-div'>
+              <img id='sundays-logo' src='/sundays.webp'></img>
+            </div>
+            <div className="company-desc-div">
+            <div id='company-desc-sundays' className="company-desc-item">
+                <h1>Results</h1>
+                <h1>Pending . . .</h1>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
